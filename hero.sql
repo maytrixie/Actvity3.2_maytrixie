1560c3ff8ff55e931d2319be070be23d12df7bf0
maytrixie <maytrixiemanumbali@gmail.com>
2023-11-06 15:17:22 +0800

    feat add column price

--- hero.sql
+++ hero.sql
@@ -149,3 +149,6 @@ VALUES
     (9, 9, 109),  -- Leomord has Blade Armor
     (10, 10, 110);  -- Chou has Feather of Heaven
 
+--add-column-price
+ALTER TABLE public.item
+ADD COLUMN item_price DECIMAL(10, 2);
