6d2403693f534e30d91f13569202c71bee97f01b
maytrixie <maytrixiemanumbali@gmail.com>
2023-11-06 15:35:16 +0800

    feat select hero archers

--- hero.sql
+++ hero.sql
@@ -167,3 +167,8 @@ SELECT p.player_name, h.hero_name
 FROM public.player p
 INNER JOIN public.hero h ON p.hero_id = h.hero_id
 WHERE h.is_active = true;
+
+-- Select hero archers
+SELECT class_name
+FROM public.class
+WHERE class_name LIKE '%Archers%';
